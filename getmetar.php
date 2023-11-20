774235ba6b17a3aaa9c45622803eadaa94ad3f8e
Dimitar Angelov <funkamateur@gmail.com>
2023-11-20 22:09:54 +0100

    getMetar updates

--- getmetar.php
+++ getmetar.php
@@ -1,29 +1,18 @@
 <?php
 
-$stations = ['LBWN', 'LBBG', 'LBSF', 'EDHI', 'EHAM'];
+$stationLines = [];
+$stations = ['LBWN', 'LBBG', 'LBSF'];
 $source = "https://metar.vatsim.net/metar.php?id=all";
-
 $allMetar = file_get_contents($source);
 $escposTemplate = file_get_contents("metar.esc");
-
-$localTime = date("H:i");
-$localTime .= 'L';
-
+$localTime = date("H:i").'L';
+$metarData = null;
 $lines = explode("\n", $allMetar);
 
-$stationLines = [];
-$metarData = null;
-
 foreach ($lines as $line)
-{
     foreach ($stations as $station)
-    {
         if (strpos($line, $station) === 0)
-        {
             array_push($stationLines, $line);
-        }
-    }
-}
 
 foreach ($stationLines as $entry)
     $metarData .= '"'.$entry.'" LF'.PHP_EOL;
